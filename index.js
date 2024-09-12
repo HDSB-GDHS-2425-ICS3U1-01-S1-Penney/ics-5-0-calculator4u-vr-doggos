1215a03e2c892206950e0dded0e776d397daa538
vr-doggos <1mcclurecam@hdsb.ca>
2024-09-12 12:46:41 -0400

    Update index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("Welcome to Another Calculator app!")
+
+let numb1 = prompt("Hi please input the first number")
+if (numb1 == "0" || numb1 == "1" || numb1 == "2" || numb1 == "3" || numb1 == "4" || numb1 == "5" || numb1 == "6" || numb1 == "7" ||numb1 == "8" ||numb1 == "9" ){
+    console.log ("Your first Number works")
+}
+
+let opperation1 = prompt ("The opperation goes here. To divide use a backslash/ to multiply use a star*")
+if (opperation1 == "-" || opperation1 == "-" || opperation1 == "+" || opperation1 == "*" || opperation1 == "x" || ){
+    console.log ("Opperation sus
+    sesful")
+}
+
+let numb2 = prompt ("Please input the second number")
+
+let numb3 = numb1 - numb2
+console.log(numb3)
